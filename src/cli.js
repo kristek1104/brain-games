7c4943b96d7d736267249e385d0df80b8d878b03
Kristina Martsinkevich <kristek1104@gmail.com>
2022-10-19 19:29:27 +0400

    brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const askAboutName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi, ${userName}!`);
+export const askAboutName = () => {
+	const userName = readlineSync.question('May I have your name? ');
+	console.log(`Hi, ${userName}!`);
 };
-export default askAboutName;
