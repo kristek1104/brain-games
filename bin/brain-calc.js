48baacf2b62c1f67843169b4ef08703cb64bd58c
Kristina Martsinkevich <kristek1104@gmail.com>
2022-10-25 21:45:37 +0400

    fix errors

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { greeting, getName, userName } from '../src/index.js';
+import { greeting } from '../src/index.js';
 import { calcGame } from '../src/calcGame.js';
-// import { calcGame } from '../src/calcGame.js';
+
 greeting();
 calcGame();
--- src/calcGame.js
+++ src/calcGame.js
@@ -1,47 +1,43 @@
 import readlineSync from 'readline-sync';
-import { getName, userName, greeting, randomNumber } from './index.js';
+import { userName, randomNumber } from './index.js';
 
-//const number1 = randomNumber();
-//const number2 = randomNumber();
 const symbols = ['+', '-', '*'];
-//const randomSymbol = symbols[Math.floor(Math.random() * 3)]
 export const rules = 'What is the result of the expression?';
-//const question = `Question: ${number1} ${randomSymbol} ${number2}`;
 
 export const calcGame = () => {
-//	console.log(rules);
-	for (let i = 0; i < 3; i += 1) {
-		const randomSymbol = symbols[Math.floor(Math.random() * 3)]
-		const number1 = randomNumber();
-		const number2 = randomNumber();
-		const question = `Question: ${number1} ${randomSymbol} ${number2}`;
-		console.log(question);
-		const userAnswer = readlineSync.question('Your Answer: ');
+  console.log(rules);
+  for (let i = 0; i < 3; i += 1) {
+    const randomSymbol = symbols[Math.floor(Math.random() * 3)];
+    const number1 = randomNumber();
+    const number2 = randomNumber();
+    const question = `Question: ${number1} ${randomSymbol} ${number2}`;
+    console.log(question);
+    const userAnswer = readlineSync.question('Your Answer: ');
 
-		const sum = number1 + number2;
-		const min = number1 - number2;
-		const mul = number1 * number2;
-		
-		const check1 = (randomSymbol === '+' && userAnswer === String(sum));
-		const check2 = (randomSymbol === '-' && userAnswer === String(min));
-		const check3 = (randomSymbol === '*' && userAnswer === String(mul));
+    const sum = number1 + number2;
+    const min = number1 - number2;
+    const mul = number1 * number2;
 
-		if (check1 === true || check2 === true || check3 === true) {
-			console.log('Correct!');
-		} else {
-			let result = 0;
-			if (randomSymbol === '+') {
-				result += sum;
-			}
-			if (randomSymbol === '-') {
-				result += min;
-			}
-			if (randomSymbol === '*') {
-				result += mul;
-			}
-			return console.log(`'${ userAnswer }' is wrong answer ;(. Correct answer was '${result}'. Let's try again, ${userName}!`);
-		}
-	}
+    const check1 = (randomSymbol === '+' && userAnswer === String(sum));
+    const check2 = (randomSymbol === '-' && userAnswer === String(min));
+    const check3 = (randomSymbol === '*' && userAnswer === String(mul));
 
-	console.log(`Congratulations, ${userName}!`);
+    if (check1 === true || check2 === true || check3 === true) {
+      console.log('Correct!');
+    } else {
+      let result = 0;
+      if (randomSymbol === '+') {
+        result += sum;
+      }
+      if (randomSymbol === '-') {
+        result += min;
+      }
+      if (randomSymbol === '*') {
+        result += mul;
+      }
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`);
+    }
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
 };
